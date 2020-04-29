cf42226b57017594e602338288b8b75ab4ed6556
danwey <509070768@qq.com>
2020-04-29 14:30:44 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwifterSwift",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v9),
         .tvOS(.v9),
         .watchOS(.v2),
         .macOS(.v10_10)
